f2dba7de328338e8c6f16bf07a9421d4da597d60
John Emilio <jl.emilio1@gmail.com>
2023-01-20 12:00:05 -0500

    tested and working solution for LoanCalculator

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class LoanCalculator {
 
     /**
-     * TODO: finish out this JavaDoc comment block.
-     * FIXME: WHAT DOES THIS METHOD DO?
-     * 
-     * @param ???
-     * @return ???
+     * This method will take in an integer as an input and will calculate the total amount of the loan remaining after three months.
+     * The method can be used in three month increments but by simply changing the totalPayments argument in the while loop, it can be used for various lengths of time.
+     *
+     * @param amount is an integer for the total loan amount
+     * @return amountRemaining is a double that will hold the total amount of the loan remaining after payments. This is declared as a double, but converted to an integer using TypeCasting
      */
     static int getRemainingAmountIn3Months(int amount) {
-        // TODO: Rewrite this method
-        return -1;
+        int totalPayments = 0;
+        double amountRemaining = amount;
+        while (totalPayments < 3 || amountRemaining <= 0){
+            amountRemaining = amountRemaining - (amountRemaining * .1);
+            totalPayments++;
+        }
+        return (int)amountRemaining;
     }
 
     public static void main(String[] args) {
